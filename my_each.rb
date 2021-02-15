3117da4b0e69669b204b06ee0ef4e13335f7adc6
rheadp <rheadpea@msu.edu>
2021-02-14 20:26:22 -0500

    commit tree 5f06086376f77546404d64fa58c7a564de9afde9

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.length
+      yield array [i]
+      i += 1
+  end
+  array
+end
+
+set = [1, 2, 3, 4, 5]
+my_each(set) do |i|
+   i
 end
